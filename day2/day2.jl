c5fdd8e727d65048044131e25f30a3800dc572d8
when-idle <when.discord@protonmail.com>
2021-12-06 01:36:46 +0000

    day2 part2 done

--- day2/day2.jl
+++ day2/day2.jl
@@ -3,18 +3,36 @@ using DelimitedFiles
 input = readdlm("input.txt")
 
 coord = [0, 0]
+p = 0;
 
-for i = 1:size(input, 1)
-	movement = input[i, 1]
-	moves    = input[i, 2]
-	if movement == "forward"
-		coord[1] += moves
-	elseif movement == "up"
-		coord[2] -= moves
-	else
-		coord[2] += moves
+function part1(coord, move)
+	for i = 1:size(input, 1)
+		movement = input[i, 1]
+		moves    = input[i, 2]
+		if movement == "forward"
+			coord[1] += moves
+		elseif movement == "up"
+			coord[2] -= moves
+		else
+			coord[2] += moves
+		end
 	end
-	
 end
+
+function part2(coord, aim)
+	for i = 1:size(input, 1)
+		movement = input[i, 1]
+		moves    = input[i, 2]
+		if movement == "forward"
+			coord[1] += moves
+			coord[2] += aim * moves 
+		elseif movement == "up"
+			aim -= moves
+		else
+			aim += moves
+		end
+	end
+end
+part2(coord, p)
 println(coord[1]*coord[2])
 
