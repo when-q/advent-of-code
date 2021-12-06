fc06f61e359749cc1e787036e25e238ff1ef15e9
when-idle <when.discord@protonmail.com>
2021-12-06 02:14:57 +0000

    day1 part1 done

--- day1/day1.jl
+++ day1/day1.jl
@@ -1,18 +1,37 @@
 @time begin
 input = parse.(Int, readlines("input.txt"))
 
-current = 0;
-larger = 0;
-for i = 1:length(input)
-	if i == 1
-		global current = input[1]
-	else
-		if input[i] > current
-			global larger += 1
+# not so smart way of doing part 1
+function part1()
+	current = 0
+	larger = 0
+	for i = 1:length(input)
+		if i == 1
+			current = input[1]
+		else
+			if input[i] > current
+				larger += 1
+			end
+			current = input[i]
 		end
-		global current = input[i]
 	end
+	return larger
 end
 
-println(larger)
+# smarter way of doing part 1
+function part1_smart(input)
+
+	solution = sum(next > prev for (prev, next) in zip(input, input[2:end]))
+	return solution
+end 
+
+# smarter way of doing part 2
+function part2(input)
+
+	solution = sum(next > prev for (prev, next) in zip(input, input[4:end]))
+	return solution
+end
+
+
+println(part2(input))
 end
