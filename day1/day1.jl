bc80cdadf58d6b64191c1e0da137b0356b5d4f36
when-idle <when.discord@protonmail.com>
2021-12-06 00:43:04 +0000

    day1 finished

---
+++ day1/day1.jl
@@ -0,0 +1,16 @@
+input = parse.(Int, readlines("input.txt"))
+
+current = 0;
+larger = 0;
+for i = 1:length(input)
+	if i == 1
+		global current = input[1]
+	else
+		if input[i] > current
+			global larger += 1
+		end
+		global current = input[i]
+	end
+end
+
+println(larger)
