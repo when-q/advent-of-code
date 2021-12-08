57fb65009d307583a532e45100770a4c6a54187f
when-idle <when.discord@protonmail.com>
2021-12-08 23:16:20 +0000

    Day5 finished

--- aoc/src/day5.jl
+++ aoc/src/day5.jl
@@ -1,30 +1,182 @@
 module day5
 
 using ..Test
-using LinearAlgebra
-struct Lines
-	x::Int64
-	y::Int64
-	z::Int64
-	w::Int64
-	Lines(x,y,z,w) = new(x, y, z, w)
-end
 
 function parseIO(io::IO)
 
-	temp = [[replace(y, "->" => "", "," =>"")] for y in readlines(io)]
-	input = [[parse(Int, ss) for ss in split(y)] for y in temp]
+	temp = [[replace(y, "->" => "", "," =>" ")] for y in readlines(io)]
+	input = [[parse(Int, ss) for ss in split(y[1])] for y in temp]
 
 	return input
 end
 
-# sus
-function isVents()
-	
-end
 
 function part1(io::IO)
+
+	v = Vector{Tuple{Int64,Int64}}()
+	visited = Vector{Tuple{Int64,Int64}}()
+
+	count = 0 
 	input = parseIO(io)
+	for line in input
+		if line[1] == line[3] # vertical
+			if line[2] <= line[4]
+				for i in line[2] : line[4]
+					if ((line[1], i) in v) && ((line[1], i) in visited) == false
+						count+=1
+						push!(visited, (line[1], i))
+						continue
+					else
+						push!(v, (line[1], i))
+					end
+				end
+			else
+				for i in line[4]: line[2]
+					if ((line[1], i) in v) && ((line[1], i) in visited) == false
+						count+=1
+						push!(visited, (line[1], i))
+						continue
+					else
+						push!(v,(line[1], i))
+					end
+				end
+			end
+
+		elseif line[2] == line[4]
+			if line[1] <= line[3]
+				for j in line[1] : line[3]
+					if ((j, line[2]) in v )&& ((j, line[2]) in visited) == false
+						count+=1
+						push!(visited, (j, line[2]))
+						continue
+					else
+						push!(v,(j, line[2]))
+					end
+				end
+			else
+				for j in line[3] : line[1]
+					if ((j, line[2]) in v )&& ((j, line[2]) in visited) == false
+						count+=1
+						push!(visited, (j, line[2]))
+						continue
+					else
+						push!(v,(j, line[2]))
+					end
+				end
+
+			end
+		end
+	end
+
+	# no more smart way
+	return count
+end
+
+function part2(io::IO)
+	count = 0 
+	input = parseIO(io)
+
+	v = Vector{Tuple{Int64,Int64}}()
+	visited = Vector{Tuple{Int64,Int64}}()
+
+	for line in input
+		if line[1] == line[3] # vertical
+			if line[2] <= line[4]
+				for i in line[2] : line[4]
+					if ((line[1], i) in v) && ((line[1], i) in visited) == false
+						count+=1
+						push!(visited, (line[1], i))
+						continue
+					else
+						push!(v, (line[1], i))
+					end
+				end
+			else
+				for i in line[4]: line[2]
+					if ((line[1], i) in v) && ((line[1], i) in visited) == false
+						count+=1
+						push!(visited, (line[1], i))
+						continue
+					else
+						push!(v,(line[1], i))
+					end
+				end
+			end
+
+		elseif line[2] == line[4]
+			if line[1] <= line[3]
+				for j in line[1] : line[3]
+					if ((j, line[2]) in v )&& ((j, line[2]) in visited) == false
+						count+=1
+						push!(visited, (j, line[2]))
+						continue
+					else
+						push!(v,(j, line[2]))
+					end
+				end
+			else
+				for j in line[3] : line[1]
+					if ((j, line[2]) in v )&& ((j, line[2]) in visited) == false
+						count+=1
+						push!(visited, (j, line[2]))
+						continue
+					else
+						push!(v,(j, line[2]))
+					end
+				end
+
+			end
+		else
+			if line[1] >= line[3] && line[2] >= line[4]
+				for j in 0 :line[1] -  line[3]
+					if ((j+line[3], j+line[4]) in v ) && ((j+line[3], j+line[4]) in visited) == false
+						count+=1
+						push!(visited, (j+line[3], j+line[4]))
+						continue
+					else
+						push!(v,(j+line[3], j+line[4]))
+					end
+				end
+			elseif line[1] >= line[3] && line[2] <= line[4]
+				for j in 0 :line[1] -  line[3]
+					if ((line[3]+j, line[4] - j) in v )&& ((line[3]+j, line[4] - j) in visited) == false
+						count+=1
+						push!(visited, (line[3]+j, line[4] - j))
+						continue
+					else
+						push!(v,(line[3]+j, line[4] - j))
+					end
+				end
+
+			elseif line[1] <= line[3] && line[2] >= line[4]
+				for j in  0: line[3] - line[1] 
+					if ((line[3] - j, line[4]+j) in v )&& ((line[3] - j, line[4]+j) in visited) == false
+						count+=1
+						push!(visited, (line[3] - j, line[4]+j))
+						continue
+					else
+						push!(v,(line[3] - j, line[4]+j))
+					end
+				end
+			else
+				for j in  0: line[3] - line[1] 
+					if ((line[3] - j, line[4]-j) in v )&& ((line[3] - j, line[4]-j) in visited) == false
+						count+=1
+						push!(visited, (line[3] - j, line[4]-j))
+						continue
+					else
+						push!(v,(line[3] - j, line[4]-j))
+					end
+				end
+
+			end
+
+	
+		end
+
+	end
+	return count
+
 end
 
 end
--- aoc/test/runtests.jl
+++ aoc/test/runtests.jl
@@ -4,5 +4,6 @@ using Test
 #include("testDay1.jl") 
 #include("testDay2.jl") 
 #include("testDay3.jl")
-include("testDay4.jl")
+#include("testDay4.jl")
+include("testDay5.jl")
 
